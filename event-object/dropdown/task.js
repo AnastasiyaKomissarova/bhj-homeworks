c5c88ba3ee00846836616514cf84451e4b875b24
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-08-15 23:40:08 +0300

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,17 @@
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownItems = Array.from(document.querySelectorAll('.dropdown__item'));
+
+dropdownValue.addEventListener('click', (event) => {
+    event.preventDefault();
+    dropdownList.classList.add('dropdown__list_active');
+});
+
+dropdownItems.forEach(item => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault();    
+    dropdownValue.textContent = item.textContent;
+    dropdownList.classList.remove('dropdown__list_active');
+  })
+
+})
