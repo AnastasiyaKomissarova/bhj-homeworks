bcd6983af2bc7d07fe063717d728492f5e833839
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-08-16 22:07:42 +0300

    theard commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,11 +2,11 @@ const menuLinks = document.querySelectorAll('.menu__link');
 
 for (let link of Array.from(menuLinks)) {
     link.onclick = function() {
-        const parent = link.closest('.menu__item');
-        if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+        /*const parent = link.closest('.menu__item');*/
+        if (this.parentElement.querySelector('.menu_sub').className === 'menu menu_sub') {
+            this.parentElement.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
         } else {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub'
+            this.parentElement.querySelector('.menu_sub').className = 'menu menu_sub'
         };
 
         if (link.closest('.menu_main')) {
