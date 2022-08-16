fefea13ee1664fbb8b922aee3d9e75fd9915381d
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-08-16 22:26:20 +0300

    four commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,13 +2,15 @@ const menuLinks = document.querySelectorAll('.menu__link');
 
 for (let link of Array.from(menuLinks)) {
     link.onclick = function() {
-        /*const parent = link.closest('.menu__item');*/
-        if (this.parentElement.querySelector('.menu_sub').className === 'menu menu_sub') {
-            this.parentElement.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+        const parent = link.closest('.menu__item');
+        if (parent.querySelector('.menu_sub') === null) {
+         return true; 
+        }
+        if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+            parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
         } else {
-            this.parentElement.querySelector('.menu_sub').className = 'menu menu_sub'
+            parent.querySelector('.menu_sub').className = 'menu menu_sub'
         };
-
         if (link.closest('.menu_main')) {
             return false
         };
