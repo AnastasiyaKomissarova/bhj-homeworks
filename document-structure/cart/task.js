7b2474fd6b775059d5837e2f9107032b773cf30f
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-08-29 10:43:54 +0300

    second commit cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,41 +1,44 @@
-const product = Array.from(document.querySelectorAll('.product'));
-const cartProducts = document.querySelector('.cart__products');
-const productAdd = Array.from(document.querySelectorAll('.product__add'));
-const productImage = Array.from(document.querySelectorAll('.product__image'));
+const cartProduct = document.querySelector('.cart__products');
+const productAdd = document.querySelectorAll('.product__add');
+const productQuantity = document.querySelectorAll('.product__quantity-control');
 
-productAdd.forEach(item => item.addEventListener('click', (e) => {
-  let id = e.currentTarget.closest('.product').dataset.id;
-  const quantityValue = e.currentTarget.closest('.product').querySelector('.product__quantity-value');
-  const cartProduct = Array.from(cartProducts.querySelectorAll('.cart__product'));
-  const cartProductCount = Array.from(cartProducts.querySelectorAll('.cart__product-count'));
-  
-  if (!cartProduct[id - 1]) {
-    cartProducts.insertAdjacentHTML('beforeend', `
-        <div class="cart__product" data-id="${id}">
-            <img class="cart__product-image" src="${productImage[id - 1].currentSrc}">
-            <div class="cart__product-count">${quantityValue.textContent}</div>
-        </div>
-        `);
-  } else {
-    cartProductCount[id - 1].textContent = Number(cartProductCount[id - 1].textContent) + Number(quantityValue.textContent);
-  }
-  })
-)
-product.forEach(item => {
-    const productQuantityControl = Array.from(item.querySelectorAll('.product__quantity-control'));
-    const productQuantityValue = item.querySelector('.product__quantity-value');
 
-    productQuantityControl.forEach(quantity => {
-        const quantityControl = () => {
-            if (quantity.classList.contains('product__quantity-control_inc')) {
-                productQuantityValue.textContent = +productQuantityValue.textContent + 1;
-            } else if (quantity.classList.contains('product__quantity-control_dec')) {
-                productQuantityValue.textContent = +productQuantityValue.textContent - 1;
-            }
-            if (productQuantityValue.textContent <= 1) {
-                productQuantityValue.textContent = 1;
-            } 
-        }
-        quantity.addEventListener('click', quantityControl);
-    })
-})
+for (let item of productQuantity) {
+    item.addEventListener('click', () => {
+        const productQuantityValue = item.parentElement.querySelector(".product__quantity-value");
+        if (item.classList.contains("product__quantity-control_dec") && productQuantityValue.textContent != 1) {
+            productQuantityValue.textContent--;
+        } else if (item.classList.contains("product__quantity-control_inc")) {
+            productQuantityValue.textContent++;
+        };
+    });
+};
+
+
+for (let item of productAdd) {
+    item.addEventListener("click", () => {
+
+       const Products = Array.from(cartProduct.getElementsByClassName('cart__product'));
+
+        for (let element of Products) {
+            if (element.dataset.id == item.closest(".product").dataset.id) {
+                const value = Number(element.querySelector(".cart__product-count").textContent);
+                const plusValue = Number(item.closest(".product").querySelector(".product__quantity-value").textContent.trim());
+
+                element.querySelector(".cart__product-count").textContent = value + plusValue;
+                localStorage.setItem("cartHTML", cartProduct.innerHTML);
+            return;
+            };
+        };
+
+        const product = item.closest(".product");
+       
+        const productQuantityValue = product.querySelector(".product__quantity-value").textContent.trim();
+        cartProduct.insertAdjacentHTML("beforeend", 
+            `<div class="cart__product" data-id="${product.dataset.id}">
+                <img class="cart__product-image" src="${product.querySelector(".product__image").getAttribute("src")}">
+                <div class="cart__product-count">${productQuantityValue}</div>
+            </div>`);
+            
+            });
+};
