6bda448361efc0173682b385887c3970fe90e96c
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-08-29 10:10:08 +0300

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,6 +3,8 @@ const hasTooltip = document.querySelectorAll('.has-tooltip')
 for (let item of hasTooltip) {
     const tooltip = document.createElement('div');
     tooltip.className = 'tooltip';
+     const {top, left} = item.getBoundingClientRect();
+     tooltip.setAttribute('style', `left: ${left}px; top: ${top+25}px;`);
     item.after(tooltip);
 
     item.addEventListener('click', (e) => {
@@ -14,4 +16,4 @@ for (let item of hasTooltip) {
      }
     e.preventDefault();
     })
-}
+ }
